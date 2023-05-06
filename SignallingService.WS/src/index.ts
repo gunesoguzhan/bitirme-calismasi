1307f02c396346d662894fb5833702bb158e2d5a
gunesoguzhan <gunesoguzhan@outlook.com.tr>
2023-05-06 20:49:47 +0300

    room socket handler

--- SignallingService.WS/src/index.ts
+++ SignallingService.WS/src/index.ts
@@ -1,10 +1,12 @@
 import { Server, ServerOptions } from 'socket.io'
 import * as config from './configuration.json'
+import { roomHandler } from './socketHandlers/roomHandler'
 
 const io = new Server(config.socketServer as Partial<ServerOptions>)
 
 io.on('connect', socket => {
     console.log('user connected')
+    roomHandler(io, socket)
     socket.on('disconnect', () => console.log('user disconnected.'))
 })
 
---
+++ SignallingService.WS/src/socketHandlers/roomHandler.ts
@@ -0,0 +1,18 @@
+import { Server, Socket } from 'socket.io'
+
+export const roomHandler = (io: Server, socket: Socket) => {
+    const joinRoom = ({ roomId }: { roomId: string }) => {
+        socket.join(roomId)
+        socket.to(roomId).emit('user:joined')
+        console.log(`user joined to the ${roomId}.`)
+    }
+
+    const leaveRoom = ({ roomId }: { roomId: string }) => {
+        socket.leave(roomId)
+        socket.to(roomId).emit('user:leaved')
+        console.log(`user leaved from the ${roomId}`)
+    }
+
+    socket.on('room:join', joinRoom)
+    socket.on('room:leave', leaveRoom)
+}
